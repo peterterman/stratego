11665f71c8827e2b3555cf988fb78eb20dfd1b93
Peter Hansen <peterterman@outlook.dk>
2026-07-07 18:51:58 +0200

    Move Stratego leaderboard to Simply server

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,20 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.stratego_flutter"
+    namespace = "dk.peterterman.stratego"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +25,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.stratego_flutter"
+        applicationId = "dk.peterterman.stratego"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -25,11 +34,20 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Signing with release key for Google Play.
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
---
+++ android/app/src/main/kotlin/dk/peterterman/stratego/MainActivity.kt
@@ -0,0 +1,5 @@
+package dk.peterterman.stratego
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
